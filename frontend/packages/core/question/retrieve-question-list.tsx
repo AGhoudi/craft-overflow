25e76ac0548df43a7e1e135929e0bdf3b4d6018a
Link1986 <ali.ghoudi@gmail.com>
2022-03-07 22:48:17 +0100

    added test file

---
+++ frontend/packages/core/question/retrieve-question-list.tsx
@@ -0,0 +1,16 @@
+import jest from 'jest';
+
+describe('retrieving question list', () => {
+  test('can retrieve empty question list', () => {
+    // arrange
+    // préparation du store
+
+    // act
+    // dispatch une action
+
+    // assert
+    // selector = expected
+  });
+});
+
+export {};
