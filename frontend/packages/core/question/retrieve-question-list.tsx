cc2d3710f625220baf10e4b8f90663c6e01beab7
Link1986 <ali.ghoudi@gmail.com>
2022-03-07 22:49:06 +0100

    added test file

--- frontend/packages/core/question/retrieve-question-list.tsx
+++ frontend/packages/core/question/retrieve-question-list.tsx
@@ -2,14 +2,8 @@ import jest from 'jest';
 
 describe('retrieving question list', () => {
   test('can retrieve empty question list', () => {
-    // arrange
-    // préparation du store
-
-    // act
-    // dispatch une action
-
     // assert
-    // selector = expected
+    
   });
 });
 
