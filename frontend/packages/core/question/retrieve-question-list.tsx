7d76e74ac9cf2733902f00d0672885a8794dcedb
Link1986 <ali.ghoudi@gmail.com>
2022-03-07 23:01:42 +0100

    added test file

--- frontend/packages/core/question/retrieve-question-list.tsx
+++ frontend/packages/core/question/retrieve-question-list.tsx
@@ -3,7 +3,9 @@ import jest from 'jest';
 describe('retrieving question list', () => {
   test('can retrieve empty question list', () => {
     // assert
-    
+    expect(selectors.selectAllQuestions(store.getState())).toEqual({
+      questions: [],
+    });
   });
 });
 
